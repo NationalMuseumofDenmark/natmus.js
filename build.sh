be161441a470e463276593f5c2c2f5ca04d32cce
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-13 15:00:24 +0100

    Changed to cip.js and the build script.

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ mkdir dist
 cat lib/cip.js/header.txt > dist/cip+natmus.min.js;
 $CLOSURE_COMPILER lib/cip.js/dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
 
-cd dist
-zip cip+natmus.zip cip+natmus.min.js
+cp lib/cip.js/dist/cip.js.zip dist/cip+natmus.js.zip
+zip -r dist/cip+natmus.js.zip dist/cip+natmus.min.js examples
