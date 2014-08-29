c3f6f800b7d143d5e2de44e786d78a51ce899db5
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-08-29 16:24:01 +0200

    Using a feature branch on the main project.

--- src/natmus.js
+++ src/natmus.js
@@ -3,24 +3,39 @@
  * Jens Christian Hillerup, BIT BLUEPRINT - jc@bitblueprint.com
  */
 
+if(typeof(require) != "undefined") {
+	cip = require('./../lib/cip.js/src/cip.js');
+}
+
 /**
  * The Nationalmuseet config for CIP.js
  */
 var NatMusConfig = {
-		endpoint: "http://cumulus.natmus.dk/CIP/",
-		constants: {
-				catch_all_alias: "any",
-				layout_alias: "web"
-		},
-		catalog_aliases: {
-			"Alle": "ALL",
-			"Antiksamlingen": "AS",
-			"Bevaringsafdelingen": "BA",
-			"Danmarks Middelalder og Renæssance": "DMR",
-			"Danmarks Nyere Tid": "DNT",
-			"Etnografisk samling": "ES",
-			"Frihedsmuseet": "FHM",
-			"Den Kgl. Mønt- og Medaljesamling": "KMM",
-			"Musikmuseet": "MUM"
-		}
+	endpoint: "http://cumulus.natmus.dk/CIP/",
+	constants: {
+			catch_all_alias: "any",
+			layout_alias: "web"
+	},
+	catalog_aliases: {
+		"Alle": "ALL",
+		"Antiksamlingen": "AS",
+		"Bevaringsafdelingen": "BA",
+		"Danmarks Middelalder og Renæssance": "DMR",
+		"Danmarks Nyere Tid": "DNT",
+		"Etnografisk samling": "ES",
+		"Frihedsmuseet": "FHM",
+		"Den Kgl. Mønt- og Medaljesamling": "KMM",
+		"Musikmuseet": "MUM"
+	}
 };
+
+if(typeof(exports) != "undefined") {
+	exports.config = NatMusConfig;
+	exports.CIPClient = cip.CIPClient;
+} else {
+	// TODO: This is a temporary fix - consider porting to require.js
+	window.natmus = {
+		config: NatMusConfig,
+		CIPClient: CIPClient
+	}
+}
