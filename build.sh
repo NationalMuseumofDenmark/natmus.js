415777cc52da291b4b20541ad608132a7ad17843
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-11-04 15:58:41 +0100

    Modifying the build script to use a CIP_JS_PATH, that is default the node_module version.

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 JSDOC="jsdoc";
 CLOSURE_COMPILER="closure";
+CIP_JS_PATH="node_modules/cip-js";
+#CIP_JS_PATH="../cip.js";
 
 # Build the cip.js lib
-git submodule update --init
-cd lib/cip.js
+cd $CIP_JS_PATH
 ./build.sh
 cd -
 
 rm -rf dist
 mkdir dist
 
-cat lib/cip.js/header.txt > dist/cip+natmus.min.js;
-$CLOSURE_COMPILER lib/cip.js/dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
+cat $CIP_JS_PATH/header.txt > dist/cip+natmus.min.js;
+$CLOSURE_COMPILER $CIP_JS_PATH/dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
 
-cp lib/cip.js/dist/cip.js.zip dist/cip+natmus.js.zip
+cp $CIP_JS_PATH/dist/cip.js.zip dist/cip+natmus.js.zip
 zip -r dist/cip+natmus.js.zip dist/cip+natmus.min.js examples
